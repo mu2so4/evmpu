85e6004cecfe24ded6014a6230ca2804550add4a
mu2so4 <57590918+mu2so4@users.noreply.github.com>
2021-12-08 19:13:54 +0700

    updated the test

--- tester.sh
+++ tester.sh
@@ -52,5 +52,16 @@ do
 		exit
 	fi
 done
+echo -e "\n" >>report.txt
+
+echo "prog4" | tee -a report.txt
+echo -e "#\treal\tuser\tsys" >>report.txt
+for (( i = 0; i < 6 ; i++ ))
+do
+	\time -o report.txt -a -f "$i\t%e\t%U\t%S" ./prog4 >/dev/null
+	if [[ $? -ne 0 ]]; then
+		exit
+	fi
+done
 
 echo "all done!"
