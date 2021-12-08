43f667ed70ba8f185bf657e4a6300ab8a3d4b4ce
mu2so4 <57590918+mu2so4@users.noreply.github.com>
2021-12-08 18:36:18 +0700

    updated the tester

--- tester.sh
+++ tester.sh
@@ -6,20 +6,49 @@ if [[ $? -ne 0 ]]; then
 	exit
 fi
 
+
 echo "prog0" > report.txt
 echo -e "#\treal\tuser\tsys" >>report.txt
 for (( i = 0 ; i < 6 ; i++ ))
 do
 	\time -o report.txt -a -f "$i\t%e\t%U\t%S" ./prog0
+	if [[ $? -ne 0 ]]; then
+		exit
+	fi
 done
 echo -e "\n" >>report.txt
+
 
 echo "prog1" >>report.txt
 echo -e "#\treal\tuser\tsys" >>report.txt
 for (( i = 0; i < 6 ; i++ ))
 do
 	\time -o report.txt -a -f "$i\t%e\t%U\t%S" ./prog1
+	if [[ $? -ne 0 ]]; then
+		exit
+	fi
 done
 echo -e "\n" >>report.txt
 
 
+echo "prog2" >>report.txt
+echo -e "#\treal\tuser\tsys" >>report.txt
+for (( i = 0; i < 6 ; i++ ))
+do
+	\time -o report.txt -a -f "$i\t%e\t%U\t%S" ./prog2
+	if [[ $? -ne 0 ]]; then
+		exit
+	fi
+done
+echo -e "\n" >>report.txt
+
+
+echo "prog2" >>report.txt
+echo -e "#\treal\tuser\tsys" >>report.txt
+for (( i = 0; i < 6 ; i++ ))
+do
+	\time -o report.txt -a -f "$i\t%e\t%U\t%S" ./prog3
+	if [[ $? -ne 0 ]]; then
+		exit
+	fi
+done
