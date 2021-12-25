0baa5f5d1611f4ed7bd3ebbcf061ccbbd5144bf7
ohmygod-programmer <dimantaras9@yandex.ru>
2021-12-25 22:21:12 +0700

    deleted unnecessary files

--- create.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Hello!"
-g++ main.cpp transform.h linear.cpp -o prog-linear -lopencv_core -lopencv_highgui
--- create0.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Hello!"
-g++ opencv.cpp /usr/lib/x86_64-linux-gnu/libopencv_core.a /usr/lib/x86_64-linux-gnu/libopencv_highgui.a -o open
-#g++ -o prog opencv.cpp -L/usr/lib/x86_64-linux-gnu/ –lopencv_core –lopencv_highgui
